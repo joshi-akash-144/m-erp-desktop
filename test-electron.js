a5b0b92acbe6b15e96ebdb0e69a331c3be6d8d1c
joshi-akash-144 <akashmahakali144@gmail.com>
2026-09-09 11:46:52 +0530

    delete debugging file text-electron.js

--- test-electron.js
+++
@@ -1,6 +0,0 @@
-﻿const e = require("electron");
-console.log("TYPE:", typeof e);
-console.log("KEYS:", Object.keys(e || {}).slice(0, 15));
-console.log("app:", typeof e.app);
-console.log("ipcMain:", typeof e.ipcMain);
-process.exit(0);
